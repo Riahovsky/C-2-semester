bd1d094b283af64b6d68f7c2b1fc70380ed80960
Riahovsky <riakhovskii.ns@phystech.edu>
2024-03-06 19:22:12 +0300

    Быстрая сортировка

--- week05/qsort.cpp
+++ week05/qsort.cpp
@@ -6,17 +6,40 @@
 //#define N 100
 //#endif
 using namespace std;
-void qsort(int arr[], int N) {
-    int n = N/2;
+void qsort(int *arr, int N) {
     int i = 0;
-    for (i = 0; i < n; ++i ) {
-        
+    int j = N - 1;
+    int m = arr[N/2];
+    do {
+    while(arr[i] < m) {
+        i++;
+    }
+    while(arr[j] > m) {
+        j--;
+    }
+    if (i <= j) {
+        swap(arr[i], arr[j]) ;
+
+        i++;
+        j--;
+    } } while(i <= j);
+
+    if (j > 0) {
+        qsort(arr,j+1);
+    }
+    if (i < N) {
+        qsort(&arr[i], N-i);
     }
 
 }
 
-
 int main() { 
-    int arr[10] ={0,1,7,6,4,7,4,7,2,6}
+    int arr[10] ={0,1,7,9,4,7,4,8,2,6};
+    int* arr1 = arr;
+    int i = 0;
+    qsort(arr1,10);
+    for (i = 0;i < 10;++i) {
+        cout << arr[i] << ' ' ;
+    }
     return 0;
 }
