c6f7bca7c14d12a0a5b03a7505e8d6ed354314e4
Riahovsky <riakhovskii.ns@phystech.edu>
2024-09-09 16:57:09 +0300

    3SEMESTER

---
+++ week/11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+
+
+
+
+int main() {
+  Node* tree = nullptr;
+  tree = new Node;
+  if ((tree->key == 0) and (tree->left == nullptr) and (tree->right == nullptr) and (tree->parent == nullptr)) {
+    cout << "OK" << endl;
+  } else {
+    cout << endl;
+  }
+  delete tree;
+  return 0;
+}
